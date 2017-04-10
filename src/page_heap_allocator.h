e4985497360b9e18bb400c3cdf93c813d0262045
zzkeln <zhengzhibin90@gmail.com>
2017-04-10 15:24:40 +0800

    Update page_heap_allocator.h

--- src/page_heap_allocator.h
+++ src/page_heap_allocator.h
@@ -56,14 +56,19 @@ class PageHeapAllocator {
     free_avail_ = 0;
     free_list_ = NULL;
     // Reserve some space at the beginning to avoid fragmentation.
+   //这个调用会先分配128k内存，然后赋值给New一个T对象地址，Delete再释放这个T对象，故当前状态是free_list持有一个T对象地址
+   //free_area有一个128k-sizeof(T)的起始地址，这个调用相当于默认分配一块内存
     Delete(New());
   }
 
   T* New() {
     // Consult free list
     void* result;
-    //先从free_list拿对象，如果free_list不为空，那么拿走
+    
     if (free_list_ != NULL) {
+     /*
+     如果free_list不为空，那么拿出free_list作为首节点返回给用户，更新free_list为空闲链表的下一个节点即它指向节点的内容
+     */
       result = free_list_;
       free_list_ = *(reinterpret_cast<void**>(result));
     } else {
@@ -71,6 +76,7 @@ class PageHeapAllocator {
       if (free_avail_ < sizeof(T)) {
         // Need more room. We assume that MetaDataAlloc returns
         // suitably aligned memory.
+        //向系统申请一块内存
         free_area_ = reinterpret_cast<char*>(MetaDataAlloc(kAllocIncrement));
         if (free_area_ == NULL) {
           Log(kCrash, __FILE__, __LINE__,
@@ -90,7 +96,10 @@ class PageHeapAllocator {
     return reinterpret_cast<T*>(result);
   }
 
- //删除T对象，回收这块内存，这块内存赋值给free_list
+ /*
+  删除T对象，回收这块内存，使用这块内存来存放free_list的地址，然后更新free_list为这块内存地址，这样各个空闲的内存就串联起来了为一个单链表
+  将p转换为void(**)然后取*并赋值为=free_list，即将p指向的对象的前面几个字节存储free_list的地址(类似于p->next=free_list)，然后更新free_list
+ */
   void Delete(T* p) {
     *(reinterpret_cast<void**>(p)) = free_list_;
     free_list_ = p;
@@ -101,14 +110,22 @@ class PageHeapAllocator {
 
  private:
   // How much to allocate from system at a time
-  static const int kAllocIncrement = 128 << 10; //128k大小
+  static const int kAllocIncrement = 128 << 10; //128k大小，x86的page是4k，故32个page
 
   // Free area from which to carve new objects
   char* free_area_; //空闲内存块起始地址
   size_t free_avail_;//空闲内存块大小
 
   // Free list of already carved objects
-  void* free_list_; //已回收的空闲对象（仅指向一个回收的T对象地址）
+ /*
+ 注意这里仅仅用一个free_list就像空闲节点给串联起一个单链表，free_list指向表头。具体思想时，返回的内存节点T*p，其中p执行的T对象都是空闲的
+ 可以把它的内容当作next来使用，存放下一个节点的地址，这样就串联为一个单链表。这个void* free_list类似于struct T {T* next}；然后free_list是
+ 表头
+ example:T是int，那么free_list就是一系列int*串联起来的单链表的头节点。当delete(int* p)时，将p指向的int内存存放free_list地址即当前链表首地址
+ 然后更新free_list=p，这样free_list指向单链表的头节点的地址，头节点的内容中存放下个节点的地址（返回内存是空闲的，所以头节点的内存中被利用起来了，
+ 当作next指针来使用）
+ */
+  void* free_list_; //已回收的空闲对象链表
 
   // Number of allocated but unfreed objects
   int inuse_;
