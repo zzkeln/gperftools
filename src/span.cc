e899488861c1e22be026cfeb4ac7805ef22c2a62
zzkeln <zhengzhibin90@gmail.com>
2017-04-11 17:10:59 +0800

    Update span.cc

--- src/span.cc
+++ src/span.cc
@@ -51,6 +51,8 @@ void Event(Span* span, char op, int v = 0) {
 }
 #endif
 
+//PageHeapAllocator<Span> Static::span_allocator_; 这个allocator是分配固定对象的allocator
+//分配一个Span对象，初始化内存为0，更新length和start地址
 Span* NewSpan(PageID p, Length len) {
   Span* result = Static::span_allocator()->New();
   memset(result, 0, sizeof(*result));
@@ -62,6 +64,7 @@ Span* NewSpan(PageID p, Length len) {
   return result;
 }
 
+//调用allocator释放span对象
 void DeleteSpan(Span* span) {
 #ifndef NDEBUG
   // In debug mode, trash the contents of deleted Spans
@@ -70,11 +73,13 @@ void DeleteSpan(Span* span) {
   Static::span_allocator()->Delete(span);
 }
 
+//初始化一个空链表，表头的next和prev都指向自己
 void DLL_Init(Span* list) {
   list->next = list;
   list->prev = list;
 }
 
+//从双向链表中移除span节点
 void DLL_Remove(Span* span) {
   span->prev->next = span->next;
   span->next->prev = span->prev;
@@ -82,6 +87,7 @@ void DLL_Remove(Span* span) {
   span->next = NULL;
 }
 
+//返回list表头的双向链表中节点个数
 int DLL_Length(const Span* list) {
   int result = 0;
   for (Span* s = list->next; s != list; s = s->next) {
@@ -90,6 +96,7 @@ int DLL_Length(const Span* list) {
   return result;
 }
 
+//将span节点插入双向链表的最前面，更新list指向span这个节点
 void DLL_Prepend(Span* list, Span* span) {
   ASSERT(span->next == NULL);
   ASSERT(span->prev == NULL);
