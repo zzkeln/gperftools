7484aaa3f6d0e4dd38f5ef12afdc133df72ae26a
zzkeln <zhengzhibin90@gmail.com>
2017-04-11 19:53:43 +0800

    Update span.h

--- src/span.h
+++ src/span.h
@@ -48,8 +48,8 @@ struct Span {
   Length        length;         // Number of pages in span Span页面的数量，Length的类型也是uintptr_t。
   Span*         next;           // Used when in link list Span *next和Span *prev是Span的指针，当Span位于PageHeap的free list双向链表中。
   Span*         prev;           // Used when in link list
-  void*         objects;        // Linked list of free objects
-  unsigned int  refcount : 16;  // Number of non-free objects 
+  void*         objects;        // Linked list of free objects，free object链表的表头，也是基于单个节点内容存放下个节点地址来实现的
+  unsigned int  refcount : 16;  // Number of non-free objects 当前span已经分配出去多少objects
   unsigned int  sizeclass : 8;  // Size-class for small objects (or 0)
   unsigned int  location : 2;   // Is the span on a freelist, and if so, which?
   unsigned int  sample : 1;     // Sampled object?
